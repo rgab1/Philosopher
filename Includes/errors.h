e9e983972c4c9db6c1571e20667126553917adac
rgab1 <rivaultgabin@gmail.com>
2026-07-15 00:19:30 +0200

    fix: fix circular depedency

--- Includes/errors.h
+++ Includes/errors.h
@@ -6,17 +6,16 @@
 /*   By: grivault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 22:06:52 by grivault          #+#    #+#             */
-/*   Updated: 2026/07/12 18:01:37 by grivault         ###   ########.fr       */
+/*   Updated: 2026/07/13 07:55:17 by grivault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
-// exit_error function messages
 # define ERROR_MALLOC_FAILED "Malloc failed\n"
 # define ERROR_MUTEX_INIT_FAILED "Mutex initialization failed\n"
 # define ERROR_EXPECTED_NBR_ARGS "Not the expected number of arguments\n"
-# define ERROR_ARGS_POSTIVE_NBR "Every argument has to be a positve number\n"
+# define ERROR_ARGS_POSITIVE_NBR "Every argument has to be a positve number\n"
 
 #endif
--- Includes/routine.h
+++ Includes/routine.h
@@ -6,13 +6,21 @@
 /*   By: grivault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 04:58:52 by grivault          #+#    #+#             */
-/*   Updated: 2026/07/13 05:01:07 by grivault         ###   ########.fr       */
+/*   Updated: 2026/07/14 05:39:41 by grivault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef ROUTINE_H
+# define ROUTINE_H
 
-#include <philosopher.h>
+typedef struct s_table	t_table;
+typedef struct s_philo	t_philo;
 
-typedef struct	t_table;
-typedef struct	t_philo;
-
+int		check_sim_stop(t_table *table);
 void	smart_sleep(long sleep_time, t_table *table);
+void	print_action(char *str, t_philo *philo);
+void	philo_eat(t_philo *philo);
+
+void	monitor_routine(t_philo *philos);
+void	*philosopher_routine(void *arg);
+
+#endif
