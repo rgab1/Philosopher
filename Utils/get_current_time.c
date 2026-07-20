9a26b4fc508944fec424350c89fdfb38367101d2
rgab1 <rivaultgabin@gmail.com>
2026-07-20 18:24:15 +0200

    fix: added an error for get_current_time

--- Utils/get_current_time.c
+++ Utils/get_current_time.c
@@ -6,7 +6,7 @@
 /*   By: grivault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 21:33:08 by grivault          #+#    #+#             */
-/*   Updated: 2026/07/10 21:39:36 by grivault         ###   ########.fr       */
+/*   Updated: 2026/07/15 16:26:02 by grivault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ long	get_current_time(void)
 	struct timeval	time;
 
 	if (gettimeofday(&time, NULL) == -1) // rechercher autres params de func
-	{
-		return (-1); // a gerer l'erreur gettimeofday failed
-	}
+		return (put_error(ERROR_GETTIMEOFDAY_FAILED), -1);
 	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
